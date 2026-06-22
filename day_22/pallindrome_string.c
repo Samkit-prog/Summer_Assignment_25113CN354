e5b2ca0a9db79ca4e18cc503cef1d581411460c4
Samkit <samkitjaind@gmail.com>
2026-06-22 22:19:05 +0530

    day 22 uploaded

---
+++ day_22/pallindrome_string.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+    char str[100];
+    int length=0,flag=1;
+    printf("Enter a string: ");
+    scanf("%s",str);
+    while(str[length]!='\0'){
+        length++;
+    }
+    for(int i=0;i<length/2;i++){
+        if(str[i]!=str[length-1-i]){
+            flag=0;
+            break;
+        }
+    }
+    if(flag){
+        printf("Palindrome String");
+    }
+    else{
+        printf("Not a Palindrome String");
+    }
+    return 0;
+}
